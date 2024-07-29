e1971c0816e4d6dc394ab90969485c21cf906015
Raphael Antonio Silveira Rezende <reezenderaphael@gmail.com>
2024-07-29 20:30:44 -0300

    Modificando a header, alterando a quantidades de receitas

--- project/settings.py
+++ project/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / 'statics'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 
