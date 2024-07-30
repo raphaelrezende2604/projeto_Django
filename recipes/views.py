42f54d19ce12b83827d77cc27d9c0d39fc36a3c0
Raphael Antonio Silveira Rezende <reezenderaphael@gmail.com>
2024-07-29 21:15:40 -0300

    Modificando as urls, para recipes aceitar numeros inteiros

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def home(resquest):
     return render(resquest, 'recipes/pages/home.html')
 
+def recipe(resquest , id ):
+    return render(resquest, 'recipes/pages/home.html')
+
